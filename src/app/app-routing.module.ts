658f07a4f3926e05fd080f5dc7d67a4d3e99e22b
Larissa Ferreira de Jesus <larissafj@gmail.com>
2024-12-23 13:36:19 -0300

    projeto angular

--- src/app/app-routing.module.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { FormularioComponent } from './formulario/formulario.component';
-
-export const routes: Routes = [
-  { path: '', component: FormularioComponent }, // Define o formulário como página inicial
-];
